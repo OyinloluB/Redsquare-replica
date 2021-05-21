41d75e2ffeaee672a7c62006489d581c64891425
OyinloluB <oyinloluwab@gmail.com>
2021-05-21 23:30:00 +0100

    section section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -14,12 +14,12 @@ const Home = () => {
           We deal in ideas, design and media that are category defying. When the
           world Millis, you’ve got to Vanilli.
         </h2>
-        {/* <p>
+        <p>
           Red Square believes in rigorous development of brand strategy and
           whip-smart execution. We make all sorts of things. Things that move
           markets, compel audiences and sell product. We help great brands
           create what comes next.
-        </p> */}
+        </p>
       </section>
     </div>
   );
