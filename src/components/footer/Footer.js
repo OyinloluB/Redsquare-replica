e6ee2f27bc0ec2c7fd2e3fbc19e7be91d3b4b1b6
OyinloluB <oyinloluwab@gmail.com>
2021-05-24 22:43:58 +0100

    footer done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from "./footer.module.scss";
+import instagram from "../../assets/images/icons/instagram.svg";
+import twitter from "../../assets/images/icons/twitter.svg";
+import linkedin from "../../assets/images/icons/linkedin.svg";
 
 const Footer = () => {
   return (
@@ -8,7 +11,18 @@ const Footer = () => {
         <h4>Let’s talk.</h4>
         <span>hi@rsq.com</span>
       </div>
-      <div className={styles.footer_rhs}></div>
+      <div className={styles.footer_rhs}>
+        <div className={styles.footer_socials}>
+          <img src={instagram} alt="instagram" />
+          <img src={twitter} alt="twitter" />
+          <img src={linkedin} alt="linkedin" />
+        </div>
+        <div className={styles.footer_locations}>
+          <h5>Mobile, AL</h5>
+          <h5>Chicago, IL</h5>
+          <h5>Tulsa, OK</h5>
+        </div>
+      </div>
     </div>
   );
 };
