72d29e9507286ce397395b3e837b8420bf7e342d
OyinloluB <oyinloluwab@gmail.com>
2021-05-26 00:00:35 +0100

    cta-footer

--- src/components/cta/Cta.js
+++ src/components/cta/Cta.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./cta.module.scss";
 
 import heroFooter from "../../assets/images/home/hero-images/hero-footer.jpg";
+import arrowRight from "../../assets/images/icons/arrow-right.svg";
 
 const Cta = () => {
   return (
@@ -9,7 +10,15 @@ const Cta = () => {
       <div className={styles.cta_wrapper}>
         <img src={heroFooter} className={styles.cta_hero} alt="hero-footer" />
       </div>
-      <div className={styles.cta_content}></div>
+      <div className={styles.cta_content}>
+        <div className={styles.cta_text}>
+          <h3>All Projects</h3>
+        </div>
+        <div className={styles.cta_cta}>
+          <p>Work</p>
+          <img src={arrowRight} alt="arrow-right" />
+        </div>
+      </div>
     </div>
   );
 };
